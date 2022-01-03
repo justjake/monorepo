05c7f0dc59f9c304625e75c0917b6d3dd27afbc1
Jake Teton-Landis <just.1.jake@gmail.com>
2022-01-02 22:22:58 -0800

    scheduler stuff

---
+++ packages/state/src/lib/Scheduler.ts
@@ -0,0 +1,127 @@
+import { Atom } from 'jotai';
+import { ReadCapability, SubscribeCapability } from '..';
+
+type AnyAtom = Atom<unknown>;
+type Effect = () => void;
+
+// Eg, setTimeout(() => {}, 0);
+export type ScheduleFn = (effect: Effect) => void;
+
+interface SchedulerConfig {
+  scheduleCompute: ScheduleFn;
+  scheduleEffect: ScheduleFn;
+  capabilities: ReadCapability & SubscribeCapability;
+}
+
+class AtomChangeSubscription {
+  prev: unknown;
+  unsubscribe: Effect;
+  listeners = new Set<Effect>();
+  constructor(public atom: AnyAtom, public config: SchedulerConfig) {
+    this.prev = this.config.capabilities.get(atom);
+    this.unsubscribe = this.config.capabilities.subscribe(
+      atom,
+      this.handleAtomMaybeChange
+    );
+  }
+  recompute = () => {
+    const cur = this.config.capabilities.get(this.atom);
+    if (!Object.is(cur, this.prev)) {
+      this.prev = cur;
+      this.listeners.forEach(this.config.scheduleEffect);
+    }
+  };
+  handleAtomMaybeChange = () => {
+    this.config.scheduleCompute(this.recompute);
+  };
+}
+
+export class ChangeSubscriber {
+  private subscriptions = new Map<AnyAtom, AtomChangeSubscription>();
+  constructor(private config: SchedulerConfig) {}
+
+  subscribeChangeEffect(atom: AnyAtom, callback: Effect): () => void {
+    let changeTracker = this.subscriptions.get(atom);
+    if (!changeTracker) {
+      changeTracker = new AtomChangeSubscription(atom, this.config);
+      changeTracker.unsubscribe = this.config.capabilities.subscribe(
+        atom,
+        changeTracker.handleAtomMaybeChange
+      );
+    }
+
+    changeTracker.listeners.add(callback);
+
+    return () => {
+      if (!changeTracker) {
+        throw new Error('unreachable');
+      }
+
+      changeTracker.listeners.delete(callback);
+      if (changeTracker.listeners.size === 0) {
+        changeTracker.unsubscribe();
+        this.subscriptions.delete(atom);
+      }
+    };
+  }
+}
+
+export class Scheduler {
+  private readonly computeQueue = new Set<Effect>();
+  private readonly effectQueue = new Set<Effect>();
+  private queued = false;
+
+  constructor(
+    private config: {
+      scheduleFlush: ScheduleFn;
+      batchEffects: ScheduleFn;
+      onError: (error: unknown) => void;
+    }
+  ) {
+    this.config = config;
+  }
+
+  scheduleCompute: SchedulerConfig['scheduleCompute'] = (effect) => {
+    this.computeQueue.add(effect);
+    this.enqueue();
+  };
+
+  scheduleEffect: SchedulerConfig['scheduleEffect'] = (effect) => {
+    this.effectQueue.add(effect);
+    this.enqueue();
+  };
+
+  flush = () => {
+    do {
+      const toCompute = [...this.computeQueue];
+      this.computeQueue.clear();
+      for (const effect of toCompute) {
+        try {
+          effect();
+        } catch (error) {
+          this.config.onError(error);
+        }
+      }
+
+      const effects = [...this.effectQueue];
+      this.effectQueue.clear();
+      this.config.batchEffects(() => {
+        for (const effect of effects) {
+          try {
+            effect();
+          } catch (error) {
+            this.config.onError(error);
+          }
+        }
+      });
+    } while (this.computeQueue.size > 0 || this.effectQueue.size > 0);
+    this.queued = false;
+  };
+
+  enqueue() {
+    if (!this.queued) {
+      this.queued = true;
+      this.config.scheduleFlush(this.flush);
+    }
+  }
+}
---
+++ packages/state/src/lib/World.ts
@@ -0,0 +1,60 @@
+import { createStore, Store } from 'jotai';
+import { jotaiStoreCapabilities, TopLevelCapabilities } from '..';
+import { ChangeSubscriber, ScheduleFn, Scheduler } from './Scheduler';
+import * as ReactDOM from 'react-dom';
+
+export class World {
+  store: Store;
+  capabilities: ReturnType<typeof jotaiStoreCapabilities>;
+  scheduler: Scheduler;
+  changeSubscriber: ChangeSubscriber;
+
+  constructor(
+    private config: {
+      onError: (error: unknown) => void;
+      scheduleFlush: ScheduleFn;
+      batchEffects: ScheduleFn;
+    }
+  ) {
+    this.store = createStore();
+    this.capabilities = jotaiStoreCapabilities(this.store);
+    this.scheduler = new Scheduler(this.config);
+    this.changeSubscriber = new ChangeSubscriber({
+      capabilities: this.capabilities,
+      scheduleCompute: this.scheduler.scheduleCompute,
+      scheduleEffect: this.scheduler.scheduleEffect,
+    });
+  }
+
+  static syncWorld() {
+    return new this({
+      batchEffects: (effects) => effects(),
+      scheduleFlush: (flush) => flush(),
+      onError(error) {
+        throw error;
+      },
+    });
+  }
+
+  static reactWorld() {
+    const scheduleFlush: ScheduleFn =
+      typeof window === 'object'
+        ? window.requestAnimationFrame
+        : (flush) => flush();
+    const onError =
+      typeof window === 'object'
+        ? (error: unknown) => {
+            // Browser: throw async. This should reach the developer console, but
+            // not block the compute queue.
+            new Promise((resolve, reject) => reject(error));
+          }
+        : (error: unknown) => {
+            throw error;
+          };
+    return new this({
+      batchEffects: ReactDOM.unstable_batchedUpdates,
+      scheduleFlush,
+      onError,
+    });
+  }
+}
